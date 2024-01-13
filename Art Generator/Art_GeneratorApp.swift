55c1a30eb27c5799d36b949c2882599dcadb6d27
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-13 15:57:49 +0300

    Initial Commit

---
+++ Art Generator/Art_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_GeneratorApp.swift
+//  Art Generator
+//
+//  Created by Omer Cagri Sayir on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Art_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
