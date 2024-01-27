1b8655479538ae9e3de19710a2796e4dcd4de77b
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-27 14:04:24 +0300

    star rotation view

--- Art Generator/Screens/FetchingView.swift
+++ Art Generator/Screens/FetchingView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct FetchingView: View {
+    @State private var rotation = 0.0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("artist")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250)
+            Text("🌟")
+                .font(.system(size: 70))
+                .offset(x: -150)
+                .rotationEffect(.degrees(rotation))
+                .animation(Animation.linear.speed(0.2).repeatForever(autoreverses: false), value: rotation)
+        }
+        .onAppear {
+            rotation = 360
+        }
     }
 }
 
