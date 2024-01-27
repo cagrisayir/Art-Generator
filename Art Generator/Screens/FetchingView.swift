615b8c028fcafc5abeadcefe007d7b84718e122e
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-27 14:03:08 +0300

    star rotation animation

---
+++ Art Generator/Screens/FetchingView.swift
@@ -0,0 +1,18 @@
+//
+//  FetchingView.swift
+//  Art Generator
+//
+//  Created by Omer Cagri Sayir on 27.01.2024.
+//
+
+import SwiftUI
+
+struct FetchingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FetchingView()
+}
